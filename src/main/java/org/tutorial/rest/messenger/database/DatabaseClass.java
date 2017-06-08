3ae1a15d726d73b5fa34bd6886e04202ef71f901
toyan <toyan@toyan-VirtualBox>
2017-06-07 22:57:57 -0400

    Rest Service Tutorial Source - https://javabrains.io/courses/javaee_jaxrs/lessons/Building-Service-Stubs

---
+++ src/main/java/org/tutorial/rest/messenger/database/DatabaseClass.java
@@ -0,0 +1,27 @@
+package org.tutorial.rest.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.tutorial.rest.messenger.model.Message;
+import org.tutorial.rest.messenger.model.Profile;
+
+
+public class DatabaseClass {
+
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<Long, Profile> profiles = new HashMap<>();
+
+	
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+	
+	public static Map<Long, Profile> getProfiles() {
+		return profiles;
+	}
+
+	
+	
+	
+}
